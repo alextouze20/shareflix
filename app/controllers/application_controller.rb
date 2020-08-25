a109234f113feebaf9a17a8b43bd2b8a2e327585
corentin bourdat <corentinbourdat@laposte.net>
2020-08-25 15:39:13 +0200

    Cloudinary est censé fonctionner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-
 before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country, :photo])
     devise_parameter_sanitizer.permit(:edit_account, keys: [:first_name, :last_name, :country])
   end
-
 end
