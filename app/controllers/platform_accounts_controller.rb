675677f1d909add2c21081a42fc7f73f71b289ba
alex tz <alextouzepro@gmail.com>
2020-08-25 16:10:00 +0200

    Correct issue with pundit

--- app/controllers/platform_accounts_controller.rb
+++ app/controllers/platform_accounts_controller.rb
@@ -1,12 +1,11 @@
 class PlatformAccountsController < ApplicationController
-  before_action :set_account, only: [:show, :edit, :destroy]
+  before_action :set_account, only: [:show, :edit]
+
   def show
-    authorize @platform_account
   end
 
   def new
     @platform_account = PlatformAccount.new
-    authorize @platform_account
   end
 
   def create
@@ -14,18 +13,15 @@ class PlatformAccountsController < ApplicationController
   end
 
   def edit
-    authorize @platform_account
   end
 
   def update
     platform = PlatformAccount.new(strong_account)
     platform.id = params[:id]
-    authorize platform_account
     platform.save
   end
 
   def destroy
-    authorize @platform_account
     PlatformAccount.destroy(@platform_account)
   end
 
