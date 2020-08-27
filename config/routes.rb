0d2b04577408f1811789abff306be8c057a75979
corentin bourdat <corentinbourdat@laposte.net>
2020-08-27 12:31:17 +0200

    ?

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,6 @@ Rails.application.routes.draw do
 
   resources :profiles, only: [:edit, :update, :show, :destroy]
 
+  end
 end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
