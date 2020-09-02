c63c995794dc01228202202c7e81345ac1e7101d
corentin bourdat <corentinbourdat@laposte.net>
2020-09-02 15:13:51 +0200

    sljsfdg

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -15,6 +15,7 @@ const initChatroomCable = () => {
           const messages = chatroom.querySelectorAll('.message-container');
           if (currentUserId == data.author_id) {
             messages[messages.length - 1].classList.add('message-from-user');
+            chatroom.scrollTop = chatroom.scrollHeight
           } else {
             messages[messages.length - 1].classList.add('message-not-user');
           }
